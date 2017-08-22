3f2a04df64e86f372afc1f51de2e69425a4134ae
GabSaldana <ela.ri.bag@outlook.com>
2017-08-22 18:40:06 -0500

    wsite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,47 +2,130 @@
 
 @section('title','Inicio')
 
-<nav class="navbar">
-  <div class="container-fluid">
-    @if (Route::has('login'))
-    <div class="navbar-header">
-      @if (Auth::check())
-      <a class="navbar-brand" href="{{ url('/home') }}">PonteEnLinea</a>
-      @else
-    </div>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="{{ url('/login') }}"><span class="fa fa-user fa-2x icono-blanco"></span> Inicia Sesión</a></li>
-      <li><a href="https://www.facebook.com/gab.saldana"><span class="fa fa-facebook-official fa-2x icono-blanco"></span> Visitanos</a></li>
-    </ul>
-    @endif
-  </div>
-  @endif
-</nav>
+<header class="top-header">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-5 header-logo">
+        <br>
+        <a href="#"><img src="img/logo.png" alt="" class="img-responsive logo"></a>
+      </div>
 
-<div class="container">
-  <div class="row">
-    <div class="jumbotron">
-      <h1 align="center"><i class="fa fa-spinner fa-pulse fa-3x fa-fw" aria-hidden="true"></i>
-        Ponte en línea
-      <i class="fa fa-users" aria-hidden="true"></i></h1>
-        <p>FALTA IMAGEN</p>
+      <div class="col-md-7">
+        <nav class="navbar navbar-default">
+          <div class="container-fluid nav-bar">
+            @if (Route::has('login'))
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+               @if (Auth::check())
+               @else
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="{{ url('/login') }}"><span class="fa fa-user fa-2x icono-blanco"></span> Inicia Sesión</a></li>
+                <li><a href="https://www.facebook.com/gab.saldana"><span class="fa fa-facebook-official fa-2x icono-blanco"></span> Visitanos</a></li>
+              </ul>
+              @endif
+          </div><!-- / .container-fluid -->
+           @endif
+        </nav>
+      </div>
     </div>
   </div>
-</div>
+</header> <!-- end of header area -->
 
-<div class="container">
-  <div class="row">
-    <div class="col-xs-12 col-sm-6 col-md-4">
-      <figure align="center"><i class="fa fa-heartbeat fa-5x icono-blanco" aria-hidden="true"></i></figure>
-      <p>El pulso cardiaco es de nuestros signos más importantes...</p>
-    </div>
-    <div class="col-xs-12 col-sm-6 col-md-4">
-      <figure align="center"> <i class="fa fa-thermometer-half fa-5x icono-blanco" aria-hidden="true"></i></figure>
-      <p>LA temperatura es el signo vital</p>
-    </div>
-    <div class="col-xs-12 col-sm-6 col-md-4">
-      <figure align="center"> <i class="fa fa-wifi fa-5x icono-blanco" aria-hidden="true"></i> </figure>
-      <p>El internet con ayuda de el pulso y la temperatura nos ayudará</p>
-    </div>
-  </div>
+<div class="container-fluid">
+
+  <section class="slider" id="home">
+		<div class="container-fluid">
+			<div class="row">
+			    <div id="carouselHacked" class="carousel slide carousel-fade" data-ride="carousel">
+					<div class="header-backup"></div>
+			        <!-- Wrapper for slides -->
+			        <div class="carousel-inner" role="listbox">
+			            <div class="item active">
+			            	<img src="img/slide-one.jpg" alt="">
+			                <div class="carousel-caption">
+		               			<h1>PONTE EN LINEA</h1>
+		               			<p>Servicio de calidad para hombres &amp; mujeres</p>
+			                </div>
+			            </div>
+			            <div class="item">
+			            	<img src="img/slide-two.jpg" alt="">
+			                <div class="carousel-caption">
+                        <h1>PONTE EN LINEA</h1>
+		               			<p>Servicio de calidad para hombres &amp; mujeres</p>
+			                </div>
+			            </div>
+			        </div>
+			        <!-- Controls -->
+			        <a class="left carousel-control" href="#carouselHacked" role="button" data-slide="prev">
+			            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			            <span class="sr-only">Previous</span>
+			        </a>
+			        <a class="right carousel-control" href="#carouselHacked" role="button" data-slide="next">
+			            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			            <span class="sr-only">Next</span>
+			        </a>
+			    </div>
+			</div>
+		</div>
+	</section><!-- end of slider section -->
+
+	<!-- service section starts here -->
+	<section class="service text-center" id="service">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 ">
+					<div class="single-service">
+						<div class="single-service-img">
+							<div class="service-img">
+								<img class="heart img-responsive" src="img/service1.png" alt="">
+							</div>
+						</div>
+						<h3>Ritmo cardiaco</br>
+            El pulso cardiaco es de nuestros signos más importantes...</h3>
+					</div>
+				</div>
+				<div class="col-md-4 ">
+					<div class="single-service">
+						<div class="single-service-img">
+							<div class="service-img">
+								<img class="brain img-responsive" src="img/service2.png" alt="">
+							</div>
+						</div>
+						<h3>Temperatura corporal</br>
+            LA temperatura es el signo vital</h3>
+					</div>
+				</div>
+				<div class="col-md-4 ">
+					<div class="single-service">
+						<div class="single-service-img">
+							<div class="service-img">
+								<img class="knee img-responsive" src="img/service3.png" alt="">
+							</div>
+						</div>
+						<h3>IOT </br>
+            El internet con ayuda de el pulso y la temperatura nos ayudará</h3>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section><!-- end of service section -->
+
+	<!-- footer starts here -->
+	<footer class="footer clearfix">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-6 footer-para">
+					<p>&copy;Gabriela Saldaña | Melina Cuadra</p>
+				</div>
+				<div class="col-xs-6 text-right">
+					<a href=""><i class="fa fa-facebook"></i></a>
+					<a href=""><i class="fa fa-twitter"></i></a>
+					<a href=""><i class="fa fa-skype"></i></a>
+				</div>
+			</div>
+		</div>
+	</footer>
+
 </div>
