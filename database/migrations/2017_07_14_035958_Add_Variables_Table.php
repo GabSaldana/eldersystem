3a3eb7c052440a2fab7c10430d9be13e5b3c635c
GabSaldana <ela.ri.bag@outlook.com>
2017-08-22 10:46:42 -0500

    añadiedno seeders para user y admin

---
+++ database/migrations/2017_07_14_035958_Add_Variables_Table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddVariablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('variables', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('range');
+            $table->string('slug')->nullable();
+            $table->rememberToken();
+            $table->softDeletes();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('variables');
+    }
+}
