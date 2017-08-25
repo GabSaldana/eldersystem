f2459eabdc66ef0f603beac970bc6b98ed60654f
GabSaldana <ela.ri.bag@outlook.com>
2017-08-25 12:40:51 -0500

    añadiendo estilo a login admin

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -1,65 +1,52 @@
-@extends('template.maindoctor')
 
+@extends('template.login')
+@section('title','login-admin')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login Doctor</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('admin.login.submit') }}">
+
+
+<div class="form">
+    <div class="tab-content">
+                <div id="login">
+                  <h1>Bienvenido!</h1>
+                    <form  method="POST" action="{{ route('admin.login.submit') }}">
                         {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">Correo Electrónico</label>
-
-                            <div class="col-md-6">
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} field-wrap">
+                            <label for="email" class="col-md-4 req">Correo Electrónico</label>
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }} field-wrap">
                             <label for="password" class="col-md-4 control-label">Contraseña</label>
-
-                            <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
-
                                 @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
-
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    ¿Olvidaste contraseña?
+                                  </a>
+                                <p class="forgot ">
+                                  <a class="btn btn-link" href="{{ route('doctor.create') }}">
+                                      Registrate!!
+                                  </a>
+                                </p>
+                                <button type="submit" class="button button-block">
                                     Entrar
                                 </button>
-
-                                <a class="btn btn-link" href="{{ route('admin.password.request') }}">
-                                    ¿Olvidaste contraseña?
-                                </a>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                              <a class="btn btn-link" href="{{ route('doctor.create') }}">
-                              Registrate!
-                              </a>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+
+
 @endsection
