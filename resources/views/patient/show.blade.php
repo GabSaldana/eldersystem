7eca6fbab71960b533acce258caee616c459a02c
melinopski <melinopski@gmail.com>
2017-09-08 13:50:31 -0500

    cambios

--- resources/views/patient/show.blade.php
+++ resources/views/patient/show.blade.php
@@ -49,17 +49,6 @@
 				@endfor
 			@endif
 	</div>
-	<div>
-		@if(Auth::guard('admin')->check())
-			<h4 style="text-align:center">Mediciones</h4>
-			<div id="areachart1" style="width:100%;margin:0 auto;"></div>
-			<div id="areachart2" style="width:100%;margin:0 auto;"></div>
-
-			@for($i=1; $i <= $count; $i++)
-				<?=$lava->render('AreaChart','Medicion'.$i,'areachart'.$i);?>
-			@endfor
-		@endif
-	</div>
-
+	
 </div>
 @endsection
