0b6e723865415dfd1a73b55a3b3fd23466261a45
GabSaldana <ela.ri.bag@outlook.com>
2017-09-30 22:30:24 -0500

    x

--- resources/views/measure/show.blade.php
+++ resources/views/measure/show.blade.php
@@ -28,7 +28,7 @@
   <div class="row">
     <div class="col-md-3"></div>
     <div class="col-md-6 show">
-      <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/338831/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Pulsos+por+minuto&type=bar&xaxis=Hora&yaxis=BPM"></iframe>
+     <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/338831/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Pulsos+por+minuto&type=bar&xaxis=Hora&yaxis=BPM"></iframe>
     </div>
     <div class="col-md-3"></div>
   </div>
