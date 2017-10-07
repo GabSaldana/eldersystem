779b35cab232766fea5a5fa3afba0dc52eb9575b
GabSaldana <ela.ri.bag@outlook.com>
2017-10-07 18:54:39 -0500

    x

--- resources/views/measure/show.blade.php
+++ resources/views/measure/show.blade.php
@@ -3,12 +3,14 @@
 @section('title', 'Mediciones')
 
 @section('content')
-
-  <h3 style="text-align:center">Mediciones</h3>
+<div class= " show colortitle" style="padding:10px;">
+  <h3 style="text-align:center;font-size:25px; ">Mediciones</h3>
+</div>
+</br>
 <div class="row"><!--PRIMERA VARIABLE DE TEMPERATURA-->
   <div class="row">
     <div class="col-md-3"></div>
-    <div class="col-md-6 show">
+    <div class="col-md-6 show" >
       <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/331178/charts/1?bgcolor=%23ffffff&color=%23d62020&days=1&dynamic=true&results=5&title=Temperatura+corporal&type=line&xaxis=Hora&yaxis=Temperatura+%C2%B0C&yaxismax=40&yaxismin=20"></iframe>
     </div>
     <div class="col-md-3"></div>
@@ -43,4 +45,13 @@
 </br>
 </div>
 
+@endsection
+@section('js')
+  <script>
+	/*ROJO, AZUL VERDE, NARANJA, MORADO*/
+	var colors2 = ['#F44336', '#0091EA', '#3F51B5', '#9C27B0', '#43A047', '#F57C00'];
+	$('.colortitle').css('color','#FFFFFF');
+	$('.colortitle').css('background-color', colors2[Math.floor(Math.random() * colors2.length)]);
+
+  </script>
 @endsection
