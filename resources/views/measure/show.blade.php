d812f2a1b4157c18b1145b16702b8dedc5b4595a
GabSaldana <ela.ri.bag@outlook.com>
2017-10-14 19:48:43 -0500

    graficas

--- resources/views/measure/show.blade.php
+++ resources/views/measure/show.blade.php
@@ -11,7 +11,7 @@
   <div class="row">
     <div class="col-md-3"></div>
     <div class="col-md-6 show" >
-      <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/331178/charts/1?bgcolor=%23ffffff&color=%23d62020&days=1&dynamic=true&results=5&title=Temperatura+corporal&type=line&xaxis=Hora&yaxis=Temperatura+%C2%B0C&yaxismax=40&yaxismin=20"></iframe>
+      <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/331178/charts/1?bgcolor=%23ffffff&color=%23d62020&days=1&dynamic=true&results=40&title=Temperatura+corporal&type=line&xaxis=Hora&yaxis=Temperatura+%C2%B0C&yaxismax=40&yaxismin=20"></iframe>
     </div>
     <div class="col-md-3"></div>
   </div>
@@ -19,7 +19,7 @@
   <div class="row">
     <div class="col-md-3"></div>
     <div class="col-md-6 show" style=" background-color:#a3ccff;">
-      <iframe width="459" height="257" style="border: 0px solid #fff; " src="https://thingspeak.com/channels/331178/maps/channel_show"></iframe>
+      <iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/338831/charts/1?bgcolor=%23ffffff&color=%23d62020&days=1&dynamic=true&results=20&title=Pulsos+por+minuto&type=bar&xaxis=Hora&yaxis=BPM"></iframe>
     </div>
     <div class="col-md-3"></div>
   </div>
